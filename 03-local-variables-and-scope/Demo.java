6f54a0038c93a96b9d2870d3c447ed2389c91052
Bunu <bunubhattarai39@gmail.com>
2024-09-10 20:02:11 -0400

    local

---
+++ 03-local-variables-and-scope/Demo.java
@@ -0,0 +1,12 @@
+package localvariablenandscope;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        int firstVariable=5;
+        int secondVariable=10;
+        if(firstVariable>0){
+            System.out.println(secondVariable);
+        }
+    }
+}
